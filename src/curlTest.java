6e5786b561aae32c51ebd300ffe4fba07660ef13
Matt Anthony <matthew.ryan.anthony@temple.edu>
2019-04-03 18:56:42 -0400

    formatted output to json

--- src/curlTest.java
+++ src/curlTest.java
@@ -1,26 +1,30 @@
 // java curl implementation
 
 
+import com.google.gson.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-
 public class curlTest {
-
 
     public static void main(String[] args) throws IOException {
 
         URL url = new URL("http://10.0.0.7:8008/setup/eureka_info");
 
-        String line;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
-            for (; (line = reader.readLine()) != null; ) {
-                System.out.println(line);
-            }
+            for (String jsonData; (jsonData = reader.readLine()) != null;) {
 
+               JsonElement jsonElement = new JsonParser().parse(jsonData);
+               Gson gson = new GsonBuilder().setPrettyPrinting().create();
+               String json = gson.toJson(jsonElement);
+               System.out.println(json);
+
+            }
         }
+
     }
 
 }
