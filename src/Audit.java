f69a1d9105fd38838fd958b98a11251c68118ad8
kevslinger <tug74186@temple.edu>
2019-04-04 22:30:56 -0400

    implementing XML parser

--- src/Audit.java
+++ src/Audit.java
@@ -2,7 +2,13 @@
 import org.nmap4j.Nmap4j;
 import org.nmap4j.core.nmap.NMapExecutionException;
 import org.nmap4j.core.nmap.NMapInitializationException;
-import org.nmap4j.data.NMapRun;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.swing.text.html.parser.Parser;
+import javax.xml.parsers.*;
+import java.io.*;
 
 public class Audit implements Runnable{
 
@@ -20,13 +26,38 @@ public class Audit implements Runnable{
         nmap4j.addFlags("-Pn");
         try {
             nmap4j.execute();
-            if (!nmap4j.hasError()) {
-                System.out.println(nmap4j.getOutput());
-            } else {
-                System.out.println(nmap4j.getExecutionResults().getErrors());
-            }
         } catch (NMapInitializationException | NMapExecutionException e){
             e.printStackTrace();
         }
+        if (!nmap4j.hasError()) {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = null;
+            ByteArrayInputStream input = null;
+            Document doc = null;
+            try {
+                builder = factory.newDocumentBuilder();
+            } catch (ParserConfigurationException e){
+                e.printStackTrace();
+                return;
+            }
+            StringBuilder xmlStringBuilder = new StringBuilder();
+            xmlStringBuilder.append(nmap4j.getOutput());
+            try {
+                input = new ByteArrayInputStream(xmlStringBuilder.toString().getBytes("UTF-8"));
+            } catch (UnsupportedEncodingException e){
+                e.printStackTrace();
+                return;
+            }
+            try {
+                doc = builder.parse(input);
+            } catch (SAXException | IOException e){
+                e.printStackTrace();
+                return;
+            }
+            //System.out.println(nmap4j.getOutput());
+        } else {
+            System.out.println(nmap4j.getExecutionResults().getErrors());
+        }
+
     }
 }
