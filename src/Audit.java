1fc9a6286faff1b7699f745edf07c1c3a26dc017
kevslinger <tug74186@temple.edu>
2019-04-05 10:05:50 -0400

    added comments to Audit

--- src/Audit.java
+++ src/Audit.java
@@ -13,6 +13,7 @@ import java.io.*;
 public class Audit implements Runnable{
 
     private String ip;
+    // NOTE: Currently, nmap4j only works for POSIX machines.
     private Nmap4j nmap4j = new Nmap4j("/usr/local");
 
     public Audit(String ip){
@@ -22,18 +23,23 @@ public class Audit implements Runnable{
 
     @Override
     public void run() {
+        // includeHosts adds the ip address to be audited.
         nmap4j.includeHosts(ip);
+        // -Pn treats the host as online, so we don't get blocked.
         nmap4j.addFlags("-Pn");
         try {
+            // If this execution does not have an error, the output will be an XML string.
             nmap4j.execute();
         } catch (NMapInitializationException | NMapExecutionException e){
             e.printStackTrace();
         }
         if (!nmap4j.hasError()) {
+            // Everything we do could throw an error!!
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = null;
             ByteArrayInputStream input = null;
             Document doc = null;
+            // I honestly have no idea what any of this does but it is more or less copied from GeeksForGeeks so it must be good.
             try {
                 builder = factory.newDocumentBuilder();
             } catch (ParserConfigurationException e){
@@ -54,6 +60,7 @@ public class Audit implements Runnable{
                 e.printStackTrace();
                 return;
             }
+            // This begins the XML parsing.
             Element root = doc.getDocumentElement();
             System.out.println(root.getAttribute("ports"));
             System.out.println(nmap4j.getOutput());
