587a97b31cca3eb7b5dbb11a18dd962a2d07f6d2
kevslinger <tug74186@temple.edu>
2019-04-08 11:41:55 -0400

    updated OS compatability

--- src/Audit.java
+++ src/Audit.java
@@ -107,16 +107,16 @@ public class Audit implements Runnable{
 
     // The following functions test for different Operating Systems.
     public static boolean isWindows() {
-        return (OS.indexOf("win") >= 0);
+        return (OS.contains("windows"));
     }
     public static boolean isMac() {
-        return (OS.indexOf("mac") >= 0);
+        return (OS.contains("mac"));
     }
     public static boolean isUnix() {
-        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
+        return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
     }
     public static boolean isSolaris() {
-        return (OS.indexOf("sunos") >= 0);
+        return (OS.contains("sunos");
     }
 
 }
