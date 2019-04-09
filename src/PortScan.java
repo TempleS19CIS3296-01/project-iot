4d85408783aad935c1ae021331a5e71c4cae3f4b
kevslinger <tug74186@temple.edu>
2019-04-09 14:48:53 -0400

    Made portscan class

---
+++ src/PortScan.java
@@ -0,0 +1,23 @@
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class PortScan implements Runnable{
+    private String ip;
+
+    public PortScan(String ip){
+        this.ip = ip;
+    }
+
+    @Override
+    public void run() {
+        for (int port = 1; port <= 65535; port++) {
+            try {
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress(ip, port), 1000);
+                socket.close();
+                System.out.println("IP is " + ip + " and Port " + port + " is open");
+            } catch (Exception expected) {
+            }
+        }
+    }
+}
