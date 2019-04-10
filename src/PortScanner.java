22eb199cb2109f9ea8c83a0361f5e839f2105882
kevslinger <tug74186@temple.edu>
2019-04-09 20:31:57 -0400

    Made Port Scanning multithreaded and also made hashmap for mapping ip->ports

--- src/PortScanner.java
+++ src/PortScanner.java
@@ -1,28 +1,42 @@
-import java.util.LinkedList;
-import java.net.SocketAddress;
+
 import java.net.Socket;
-import java.io.*;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
 
 public class PortScanner implements Runnable{
-    private LinkedList<Integer> ports;
+    private HashMap<String, LinkedListInt> portMap;
     private String IP;
 
-    public PortScanner(String ip){
+    public PortScanner(HashMap portMap, String ip){
+        this.portMap = portMap;
         IP = ip;
     }
     
     public void run(){
-        for(int i = 0; i < 65535; i++){
+        for(int port = 0; port < 65535; port++){
             try {
                 Socket socket = new Socket();
-                socket.connect(new InetSocketAddress(IP, i), 300);
+                socket.connect(new InetSocketAddress(IP, port), 300);
                 socket.close();
             } catch (Exception ex) {
                 continue;
             }
-            ports.add(i);
-            System.out.println("Open Port found at : " + i);
+            synchronized (portMap) {
+                LinkedListInt portList = portMap.get(IP);
+                if (portList != null){// If we already have an element for this IP address.
+                    portList.add(port);
+                    portMap.put(IP, portList);
+                } else {// We do not have any ports yet for this IP address.
+                    portList = new LinkedListInt();
+                    portList.add(port);
+                    portMap.put(IP, portList);
+                }
+            }
+            System.out.println("IP: "+ IP+ " port found at : " + port);
         }
+    }
+    // Accessor for ports.
+    public HashMap getPorts(){
+        return portMap;
     }
 }
