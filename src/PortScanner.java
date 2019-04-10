76a6614ccf715d32e1e20930bf0f67be1ab372ba
kevslinger <tug74186@temple.edu>
2019-04-10 10:33:10 -0400

    I dont know what I did

--- src/PortScanner.java
+++ src/PortScanner.java
@@ -13,26 +13,27 @@ public class PortScanner implements Runnable{
     }
     
     public void run(){
+        // omp parallel for
         for(int port = 0; port < 65535; port++){
             try {
                 Socket socket = new Socket();
                 socket.connect(new InetSocketAddress(IP, port), 300);
                 socket.close();
-            } catch (Exception ex) {
-                continue;
-            }
-            synchronized (portMap) {
-                LinkedListInt portList = portMap.get(IP);
-                if (portList != null){// If we already have an element for this IP address.
-                    portList.add(port);
-                    portMap.put(IP, portList);
-                } else {// We do not have any ports yet for this IP address.
-                    portList = new LinkedListInt();
-                    portList.add(port);
-                    portMap.put(IP, portList);
+                synchronized (portMap) {
+                    LinkedListInt portList = portMap.get(IP);
+                    if (portList != null){// If we already have an element for this IP address.
+                        portList.add(port);
+                        portMap.put(IP, portList);
+                    } else {// We do not have any ports yet for this IP address.
+                        portList = new LinkedListInt();
+                        portList.add(port);
+                        portMap.put(IP, portList);
+                    }
                 }
+                System.out.println("IP: "+ IP+ " port found at : " + port);
+            } catch (Exception ex) {
             }
-            System.out.println("IP: "+ IP+ " port found at : " + port);
+
         }
     }
     // Accessor for ports.
