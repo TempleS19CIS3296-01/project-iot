2ec183b67dafd39fe55c8f77e3fd2ec9b9f5ef05
Patrick Pettus <tuf50784@temple.edu>
2019-04-16 14:58:29 -0400

    create LinkedListInt of targetports to greatly increase scan speed

--- src/PortScanner.java
+++ src/PortScanner.java
@@ -3,23 +3,30 @@ import java.net.Socket;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 
+
 public class PortScanner implements Runnable{
     private HashMap<String, LinkedListInt> portMap;
     private String IP;
+    LinkedListInt targetPorts;
+
 
     public PortScanner(HashMap portMap, String ip){
         this.portMap = portMap;
         IP = ip;
+        targetPorts = getTargetPorts();//generate list of "high risk" ports
     }
     
     public void run(){
         // Iterate over all ports.
-        for(int port = 0; port < 65535; port++){
+        LinkedListInt.Node port = targetPorts.head.next;
+        boolean morePorts = true;
+
+        while(morePorts){
             try {
                 // Try to establish a socket connection with IP and port.
                 // TODO: timeout? Is 300 a good number?
                 Socket socket = new Socket();
-                socket.connect(new InetSocketAddress(IP, port), 300);
+                socket.connect(new InetSocketAddress(IP, port.val), 300);
                 socket.close();
                 // TODO: Do we need synchronized? no other thread will have the same IP address.
                 synchronized (portMap) {
@@ -27,22 +34,61 @@ public class PortScanner implements Runnable{
                     // Otherwise, create a new linkedlist and add that to the hashmap.
                     LinkedListInt portList = portMap.get(IP);
                     if (portList != null){// If we already have an element for this IP address.
-                        portList.add(port);
+                        portList.add(port.val);
                         portMap.put(IP, portList);
                     } else {// We do not have any ports yet for this IP address.
                         portList = new LinkedListInt();
-                        portList.add(port);
+                        portList.add(port.val);
                         portMap.put(IP, portList);
                     }
                 }
-                System.out.println("IP: "+ IP+ " port found at : " + port);
+                System.out.println("IP: "+ IP+ " port found at : " + port.val);
             } catch (Exception ex) {
             }
 
+            if(port.next == null){
+                morePorts = false;//if there are no more ports, exit while loop
+            }
+            else port = port.next;//otherwise, go to next port
         }
     }
     // Accessor for ports.
     public HashMap getPorts(){
         return portMap;
+    }
+
+    // Get linkedlist of target ports - eventually, this should be generated from our database
+    public LinkedListInt getTargetPorts(){
+        
+        
+        LinkedListInt targetPorts = new LinkedListInt();
+
+        //eventually, this is where we'll hit a database to generate a more specific list of target ports
+        targetPorts.add(20);
+        targetPorts.add(21);
+        targetPorts.add(22);
+        targetPorts.add(23);
+        targetPorts.add(25);
+        targetPorts.add(53);
+        targetPorts.add(67);
+        targetPorts.add(68);
+        targetPorts.add(69);
+        targetPorts.add(80);
+        targetPorts.add(110);
+        targetPorts.add(123);
+        targetPorts.add(137);
+        targetPorts.add(138);
+        targetPorts.add(139);
+        targetPorts.add(143);
+        targetPorts.add(161);
+        targetPorts.add(162);
+        targetPorts.add(179);
+        targetPorts.add(389);
+        targetPorts.add(443);
+        targetPorts.add(636);
+        targetPorts.add(989);
+        targetPorts.add(990);
+
+        return targetPorts;
     }
 }
