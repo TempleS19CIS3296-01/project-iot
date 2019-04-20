cd30ac5cd5dcf2c3430b6d3da318f88a1c3aa8ce
kevslinger <tug74186@temple.edu>
2019-04-20 18:23:19 -0400

    Updated curlTest to curl for google home firmware

--- src/curlTest.java
+++ src/curlTest.java
@@ -1,44 +1,27 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class curlTest {
-    //function takes in a url and returns a string of the formatted json data
-    public static String jsonConverter(URL url) {
-        String json = "";
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
-            for (String jsonData; (jsonData = reader.readLine()) != null;) {
 
-                JsonElement jsonElement = new JsonParser().parse(jsonData);
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                json = gson.toJson(jsonElement);
-                return json;
-            }
+    public static void main(String[] args) {
+        // URL which stores google home firmware version.
+        String url = "https://support.google.com/googlehome/answer/7365257?hl=en";
+        // This will be our HTML document.
+        Document doc = null;
+        try {
+            // Try to scrape the contents of the site.
+            doc = Jsoup.connect(url).get();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return json;
-    }
+        // Select the Div element with class=cc, then get the first element with "strong", then convert that to text.
+        String productionVersion = doc.select("div.cc").first().selectFirst("strong").text();
+        System.out.println(productionVersion);
 
-    public static void main(String[] args) throws MalformedURLException {
-        URL url = new URL("https://support.google.com/googlehome/answer/7365257?hl=en");
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
-            for (String line; (line = reader.readLine()) != null;) {
-                System.out.println(line);
-            }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
